76b2a713f5018112a046360cccf242f159a00fd6
vaishnavipachpor <59760241+vaishnavipachpor@users.noreply.github.com>
2023-09-06 16:39:53 +0530

    Add project files.

---
+++ Swap1/Program.cs
@@ -0,0 +1,12 @@
+﻿//write a program to swap two number without using third variable.
+class SwapNumbers1   //solution 1
+{
+    static void Main()
+    {
+        int a = 342, b = 784;
+        Console.WriteLine($"Nmuber before swap: a=> {a};b=> {b}");
+        a = a * b; b = a / b; a = a / b;
+        Console.WriteLine($" Number after swaping :a=>{a};b=>{b}");
+        Console.ReadLine();
+    }
+}
